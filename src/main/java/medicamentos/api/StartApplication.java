4de38fb1a55118b4f6bc0bc70e85accb741dbef0
Bruno Henrique <bh45237@gmail.com>
2024-07-17 00:20:42 -0300

    web client centralizado

--- src/main/java/medicamentos/api/StartApplication.java
+++ src/main/java/medicamentos/api/StartApplication.java
@@ -3,29 +3,11 @@ package medicamentos.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpHeaders;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class StartApplication {
 
-	private void configureDefaultHeaders(final HttpHeaders headers) {
-		headers.add(HttpHeaders.ACCEPT, "application/json, text/plain, */*");
-		headers.add(HttpHeaders.AUTHORIZATION, "Guest");
-		headers.add(HttpHeaders.REFERER, "https://consultas.anvisa.gov.br/");
-	}
-
-	@Bean
-	public WebClient webClient(WebClient.Builder builder) {
-		return builder
-			.baseUrl("https://consultas.anvisa.gov.br/api")
-			.defaultHeaders(this::configureDefaultHeaders)
-			.build();
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(StartApplication.class, args);
 	}
-
 }
--- src/main/java/medicamentos/api/controller/MedicamentosController.java
+++ src/main/java/medicamentos/api/controller/MedicamentosController.java
@@ -1,26 +1,32 @@
 package medicamentos.api.controller;
 
+import medicamentos.api.consumer.bulario.BularioAnvisaApiConsumer;
 import medicamentos.api.domain.anvisaApi.AnvisaApiDTO;
 import medicamentos.api.domain.medicamento.Medicamento;
 import medicamentos.api.service.MedicamentosService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/medicamentos")
 public class MedicamentosController {
 
     @Autowired
+    private BularioAnvisaApiConsumer bularioApiConsumer;
+
+    @Autowired
     private MedicamentosService service;
 
     @GetMapping
-    public AnvisaApiDTO<Medicamento> getMedicamentos(Pageable pagination) {
-        return service.getPageMedicamentos(pagination);
+    public AnvisaApiDTO<Medicamento> getMedicamentos(
+            @RequestParam(required = false, defaultValue = "") String nomeMedicamento,
+            @PageableDefault(size = 10, page = 1) Pageable pagination
+    ) {
+        return bularioApiConsumer.getMedicamentos(nomeMedicamento, pagination);
     }
-
 }
