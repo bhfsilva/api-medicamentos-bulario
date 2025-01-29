aa897beb395a732c2ff76a1c9cae1f382d1f3cb7
Bruno Henrique <bh45237@gmail.com>
2025-01-29 20:29:36 -0300

    add raise exception when error response in http service request

--- src/services/http_client.py
+++ src/services/http_client.py
@@ -1,15 +1,20 @@
 import requests
+from requests.exceptions import RequestException
 
 class HttpService:
   def __init__(self, headers: dict):
     self.headers = headers
 
   def get(self, url: str) -> dict:
-    content = {}
-    response = requests.get(url, headers=self.headers)
-    
     try:
+      content = {}
+      response = requests.get(url, headers=self.headers)    
+      response.raise_for_status()
       content = response.json()
+    
+    except RequestException as e:
+      raise Exception(f"{str(e)}")
+
     except ValueError:
       raise Exception("No JSON content")
     
